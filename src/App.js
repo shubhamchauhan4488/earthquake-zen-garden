6f288b8c9092e01d73d30c5821345eeb0c60872c
shubhamchauhan4488 <shubhamchauhan4488@gmail.com>
2021-07-15 17:17:07 -0400

    Add Appjs, router and sourceData

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
 import React from 'react';
+import Header from './components/Header';
+import Home from './components/Home';
+import { sourceData } from './sourceData';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import Profile from './components/Profile';
+import EarthquakeDetail from './components/EarthquakeDetail';
 
 const App = () => {
   return (
-    <div>Some html</div>
+    <>
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path="/">
+            <Home sourceData={sourceData} />
+          </Route>
+          <Route path="/profile">
+            <Profile profileData={sourceData.profile} />
+          </Route>
+          <Route path="/detail/:id" >
+            <EarthquakeDetail sourceData={sourceData} />
+          </Route>
+        </Switch>
+      </Router>
+    </>
   );
 };
 
